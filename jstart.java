d0c08b0d9590d91595f79bd0b885ff4bfac16094
Shizzledoink <coven3d2015@hotmail.com>
2022-03-09 20:33:21 -0700

    Change jstart boilerplate to make a bit nicer to copy and start coding

--- jstart.java
+++ jstart.java
@@ -1,11 +1,10 @@
-import java.util.Scanner;
+import java.util.*;
 public class Prob00 {
 	public static void main(String[] args) {
 		try (Scanner input = new Scanner(System.in)){
 			int testCases = Integer.parseInt(input.nextLine());
-			for(int testcase = 0; testcase < testCases; testcase++) {
-				// Actual code starts here
-				System.out.println(input.nextLine());
+			for(int testcase = 0; testcase < testCases; testcase++) { // solution starts below
+
 			}
 		}
 	}
