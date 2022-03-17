0d0ded8b7b57bf84581b16d5350c93e85d381892
Shizzledoink <coven3d2015@hotmail.com>
2022-03-16 21:06:12 -0600

    Flip Flop

--- flip flop/flipflop.java
+++ flip flop/flipflop.java
@@ -6,21 +6,41 @@ public class Prob00 {
 			for(int testcase = 0; testcase < testCases; testcase++) { // solution starts below
 				int iRows = input.nextInt();
 				int iCols = input.nextInt();
-				
+
 				int oRows = iCols;
 				int oCols = iRows;
 
 				String[][] inputArray = new String[iRows][iCols];
 
-				for(int i = 0; i <= iRows; i++) {
-					String thisRow = input.nextLine();
-					// use string replace ,, with ,<placeholder>, to have a value that can be turned back into nothing at the end.
-					//thisRow = thisRow.replace(",,", ",X,");
+				String[][] output = new String[oRows][oCols];
+
+
+				String thisRow = input.nextLine(); // I have absolutely no idea why I need it declared outside the loop like this but it worked
+				// Parse each row of stdin for the 2d input array
+				for(int i = 0; i < iRows ; i++) {
+					thisRow = input.nextLine();
 					// Split the string into an array at commas
-					String[] rowArray = thisRow.split(",");
-					System.out.println(Arrays.toString(rowArray));
-					// Convert column to an array of ints to be added to each row in this loop
+					// -1 needed to keep the trailing commas --- https://stackoverflow.com/questions/2170557/split-method-of-string-class-does-not-include-trailing-empty-strings
+					String[] rowArray = thisRow.split(",",-1);
+
+					inputArray[i] = rowArray;
 				}
+				// System.out.println(Arrays.deepToString(inputArray));
+
+				for(int row = 0; row < oRows; row++) { // for each column of each row in output, get the corrosponding opposite cords
+					for(int col = 0; col < oCols; col++){
+						output[row][col] = inputArray[col][row];
+					}
+				}
+				// System.out.println("New " + Arrays.deepToString(output));
+
+				// Print it out in the format lockheed wants
+				System.out.print( // yes
+						Arrays.deepToString(output)
+						.replaceAll("],|]]", "\n")
+						.replaceAll("\\[| ", "")
+				);
+
 			}
 		}
 	}
