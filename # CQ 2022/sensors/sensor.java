57eee743d4ab3f2f55bdbfb86eefea9417247a0a
omni <coven3d2015@hotmail.com>
2022-04-30 10:51:07 -0600

    sensor

--- # CQ 2022/sensors/sensor.java
+++ # CQ 2022/sensors/sensor.java
@@ -1,3 +1,4 @@
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class sensor {
@@ -5,6 +6,7 @@ public class sensor {
 		try (Scanner input = new Scanner(System.in)){
 			int testCases = Integer.parseInt(input.nextLine());
 			for(int testcase = 0; testcase < testCases; testcase++) { // solution starts below
+				int numSensors = Integer.parseInt(input.nextLine());
 				String inTXT = input.nextLine();
 				String[] sensorsTXT = inTXT.split(" ");
 
@@ -12,6 +14,19 @@ public class sensor {
 				int[] sensors = new int[sensorsTXT.length];
 				for (int i = 0; i < sensorsTXT.length; i++) {
 					sensors[i] = Integer.parseInt(sensorsTXT[i]);
+				}
+
+				Arrays.sort(sensors);
+
+				/* System.out.println( 
+					Arrays.toString(sensors)
+				); */
+
+
+				for(int i = 0; i < sensors.length -1 ; i++ ) {
+					if(sensors[i] != sensors[i+1] - 1) {
+						System.out.println(sensors[i] + 1);
+					}
 				}
 
 			}
